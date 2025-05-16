d6ab136514c4672f974daad2e2378d37e0ca51a8
Marek <kabzinskimilosz@gmail.com>
2025-05-16 20:47:51 +0200

    Integrate form handling with react-hook-form

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,81 +1,67 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
-import './App.css'
-import Welcome from './components/other/WelcomeAnimation/WelcomeAnimation'
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import "./App.css";
+import Welcome from "./components/other/WelcomeAnimation/WelcomeAnimation";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./components/ui/card"
-import { Input } from "./components/ui/input"
-import { Label } from "./components/ui/label"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "./components/ui/tabs"
-
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "./components/ui/card";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs";
+import JoinForm from "./components/other/JoinForm/JoinForm";
 //Animations to do
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <div className="flex flex-col gap-10 h-60 justify-between">
+                <Welcome></Welcome>
+                <div className="flex flex-row justify-center">
+                    <Tabs
+                        defaultValue="account"
+                        className="w-[400px] flex gap-5"
+                    >
+                        <TabsList className="grid w-full grid-cols-2 gap-3">
+                            <TabsTrigger value="join">
+                                Dołącz do testu
+                            </TabsTrigger>
+                            <TabsTrigger value="create">
+                                Utwórz Test
+                            </TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="join">
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>Dołącz do testu</CardTitle>
+                                </CardHeader>
 
-  return (
-    <>
-    <div className='flex flex-col gap-10 h-60 justify-between'>
-      <Welcome></Welcome>
-      <div className='flex flex-row justify-center'>
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="join">Dołącz do testu</TabsTrigger>
-          <TabsTrigger value="create">Utwórz Swój Własny Test</TabsTrigger>
-        </TabsList>
-        <TabsContent value="join">
-          <Card>
-            <CardHeader>
-              <CardTitle>Dołącz do testu</CardTitle>
-              <CardDescription>
-                Make changes to your account here. Click save when you're done.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-4">
-              <div className="space-y-1">
-                <Label htmlFor="name">Imię</Label>
-                <Input id="name" defaultValue="Jan" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="surname">Nazwisko</Label>
-                <Input id="surname" defaultValue="Kowalski" required/>
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="code">Kod Testu</Label>
-                <Input id="code" defaultValue="H1LSNMSD9COWEK" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Dołącz</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-        <TabsContent value="create">
-          <Card>
-            <CardHeader>
-              <CardTitle>Utwórz Swój Własny Test</CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-1">
-              <Button>Utwórz Test</Button>
-            </CardContent>
-          </Card>
-        </TabsContent>
-      </Tabs>
-      </div>
-    </div>
-    </>
-  )
+                                <JoinForm></JoinForm>
+                            </Card>
+                        </TabsContent>
+                        <TabsContent value="create">
+                            <Card>
+                                <CardHeader>
+                                    <CardTitle>Utwórz Test</CardTitle>
+                                </CardHeader>
+                                <CardContent className="space-y-1">
+                                    <Label htmlFor="file">Plik .csv</Label>
+                                    <Input id="file" type="file" />
+                                </CardContent>
+                                <CardFooter>
+                                    <Button>Utwórz</Button>
+                                </CardFooter>
+                            </Card>
+                        </TabsContent>
+                    </Tabs>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ ReactApp/src/components/other/JoinForm/JoinForm.tsx
@@ -0,0 +1,114 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Musisz podać imie.",
+    }),
+    surname: z.string().min(1, {
+        message: "Musisz podać nazwisko.",
+    }),
+    code: z.string().length(8, {
+        message: "Kod Testu ma 8 liter",
+    }),
+});
+
+export default function ProfileForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            surname: "",
+            code: "",
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <CardContent className="space-y-4 gap-3">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Imię</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="name"
+                                        placeholder="Jan"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="surname"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Nazwisko</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="surname"
+                                        placeholder="Kowalski"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="code"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Kod Testu</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="code"
+                                        placeholder="H1LSNMSD9COWEK"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </CardContent>
+                <CardFooter>
+                    <Button type="submit">Dołącz</Button>
+                </CardFooter>
+            </form>
+        </Form>
+    );
+}
